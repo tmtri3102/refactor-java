705a0e33f87086a6c29d6eb126980dfc9510cf2f
tmtri3102 <tmtri310251@gmail.com>
2024-10-18 11:57:20 +0700

    Update

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,5 @@
 public class FizzBuzz {
-    public static String fizzBuzz(int number){
+    public static String fizzBuzz(int number) {
 //        if(number % 3 == 0 && number % 5 == 0)
 //            return "FizzBuzz";
 //
@@ -14,14 +14,15 @@ public class FizzBuzz {
         boolean isFizz = number % 3 == 0;
         boolean isBuzz = number % 5 == 0;
 
-        if(isFizz && isBuzz)
+        if (isFizz && isBuzz)
             return "FizzBuzz";
 
-        if(isFizz)
+        if (isFizz)
             return "Fizz";
 
-        if(isBuzz)
+        if (isBuzz)
             return "Buzz";
 
         return number + "";
+    }
 }
